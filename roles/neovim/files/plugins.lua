2413639f8a9d73454421f1a27df66a3c23bf4851
VitodAlighieri <77302406+VitodAlighieri@users.noreply.github.com>
2024-06-05 01:48:17 +0200

    upgraded neovim plugins config

--- roles/neovim/files/plugins.lua
+++ roles/neovim/files/plugins.lua
@@ -17,35 +17,13 @@ local plugins = {
      end,
     },
  
-   {
-     "goolord/alpha-nvim",
-     disable = false,
-   },
- 
-   {
-     "segeljakt/vim-silicon",
-     config = function ()
-       vim.cmd [[
-         let g:silicon = {
-           \   'theme':               'OneHalfDark',
-           \   'font': 'Fira Code,Fira Code Medium',
-           \   'background':              '#282c34',
-           \   'shadow-color':            '#282c34',
-           \   'line-pad':                        2,
-           \   'pad-horiz':                       0,
-           \   'pad-vert':                        0,
-           \   'shadow-blur-radius':              0,
-           \   'shadow-offset-x':                 0,
-           \   'shadow-offset-y':                 0,
-           \   'line-number':                v:true,
-           \   'round-corner':               v:true,
-           \   'window-controls':           v:false,
-           \ }
-           let g:silicon['output'] = '/tmp/silicon-{time:%Y-%m-%d-%H%M%S}.png'
-          ]]
-       end,
+    {
+      'goolord/alpha-nvim',
+      config = function ()
+          require'alpha'.setup(require'alpha.themes.dashboard'.config)
+      end
     },
- 
+    
    {
      "neovim/nvim-lspconfig",
      config = function()
@@ -53,8 +31,6 @@ local plugins = {
        require "custom.configs.lspconfig"
      end,
    },
- 
-   {"watzon/vim-edge-template"},
  
    {
      "williamboman/mason.nvim",
