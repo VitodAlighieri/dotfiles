4b37bec7d82da500eb7f091bbbcb97c2572c6f55
bmartinez <77302406+bmartinez@users.noreply.github.com>
2024-06-05 11:00:46 +0200

    neovim config started with correct format

--- roles/neovim/files/chadrc.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.plugins = "custom.plugins"
-
-M.mappings = require "custom.configs.mappings"
-
-return M
--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,1 +1,17 @@
-vim.cmd [[ autocmd BufEnter * if &buftype != "terminal" | lcd %:p:h | endif ]]
+-- LAZY VIM INSTALLATION
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup("plugins")
