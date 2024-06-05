fb0fae4bce8bc8d2dd32569cbaf02546aff7ed35
VitodAlighieri <bielmj10@gmail.com>
2024-06-06 01:27:38 +0200

    Neovim lsp config added

--- roles/neovim/files/lua/plugins/telescope.lua
+++ roles/neovim/files/lua/plugins/telescope.lua
@@ -1,8 +1,27 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    end
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			require("telescope").load_extension("ui-select")
+    end,
+  },
 }
