9e6ad4cef68ad00f68c63f8fa510f005bef31975
VitodAlighieri <bielmj10@gmail.com>
2024-06-06 13:51:25 +0200

    Neovim lsp config arranged and easy waybar setup included. Ready for customization

--- roles/neovim/files/lua/plugins/lsp-config.lua
+++ roles/neovim/files/lua/plugins/lsp-config.lua
@@ -25,11 +25,11 @@ return {
         capabilities = capabilities
       })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+      		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
 		end,
   	},
 }
