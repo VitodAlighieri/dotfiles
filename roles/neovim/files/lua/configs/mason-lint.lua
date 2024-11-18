70d7143fe7cca2b2a7ae5c57b564744d12380240
VitodAlighieri <bielmj10@gmail.com>
2024-11-18 21:24:02 +0100

    Added linters and some configuration to them

---
+++ roles/neovim/files/lua/configs/mason-lint.lua
@@ -0,0 +1,24 @@
+local lint = package.loaded["lint"]
+
+local function table_contains(table, value)
+  for _, v in ipairs(table) do
+    if v == value then
+      return true
+    end
+  end
+  return false
+end
+
+local all_linters = {}
+for _, v in pairs(lint.linters_by_ft) do
+  for _, linter in ipairs(v) do
+    if not table_contains(all_linters, linter) then
+      table.insert(all_linters, linter)
+    end
+  end
+end
+
+require("mason-nvim-lint").setup({
+  ensure_installed = all_linters,
+  automatic_installation = true,
+})
--- roles/neovim/files/lua/plugins/init.lua
+++ roles/neovim/files/lua/plugins/init.lua
@@ -23,4 +23,21 @@ return {
       },
     },
   },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require "configs.lint"
+    end,
+  },
+
+  {
+    "rshkarin/mason-nvim-lint",
+    event = "VeryLazy",
+    dependencies = { "nvim-lint" },
+    config = function()
+      require "configs.mason-lint"
+    end,
+  },
 }
